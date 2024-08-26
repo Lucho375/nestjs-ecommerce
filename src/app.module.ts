4b80c9eacf8ca9b1a1559816539826aa8e280a70
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-08-26 18:49:24 -0300

    feat/auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AppController],
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     ProductsModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs-ecommerce'),
+    AuthModule,
   ],
 })
 export class AppModule {}
