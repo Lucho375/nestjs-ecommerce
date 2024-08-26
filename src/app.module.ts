eb8a55ec00b8cab20a9f19386787df59ffe99b91
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-08-26 19:11:02 -0300

    merge conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-
+import { HashModule } from './hash/hash.module';
+import { HashService } from './hash/hash.service';
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HashService],
   imports: [
     ProductsModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs-ecommerce'),
     AuthModule,
+    HashModule,
   ],
 })
 export class AppModule {}
