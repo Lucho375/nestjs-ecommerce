f12312fd88e750bdb82a60159da0cec59c336ddc
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-08-27 20:20:10 -0300

    fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HashModule } from 'src/hash/hash.module';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
-import { HashService } from 'src/hash/hash.service';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    HashModule,
   ],
-  providers: [UsersService, HashService],
+  providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController],
 })
