099ad4362d7aecbc83aa5c642fb8d9733d8d2b45
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-08-29 19:08:53 -0300

    feat/cloudinary

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryResponse } from './cloudinary-response';
+import { Readable } from 'stream';
+
+@Injectable()
+export class CloudinaryService {
+  async uploadFile(
+    file: Express.Multer.File,
+    folder: string,
+  ): Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          transformation: [{ quality: 'auto:good', fetch_format: 'webp' }],
+          folder,
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+
+      const bufferStream = new Readable();
+      bufferStream.push(file.buffer);
+      bufferStream.push(null);
+      bufferStream.pipe(stream);
+    });
+  }
+}
