4aaf4fc1f7d0509f5c9ad30a60f59141fa3e58c6
Luciano Rodríguez <luciano.rodriguez59@gmail.com>
2024-09-09 19:40:51 -0300

    feat/orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { User } from 'src/common/decorators/user.decorator';
+import { Role } from 'src/common/enums/roles.enum';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Get()
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
   @Get(':id')
-  @HttpCode(HttpStatus.OK)
-  getOrder(@Param('id') orderId: string) {
-    return this.ordersService.getOrder(orderId);
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN)
+  findOne(@Param('id') orderId: string) {
+    return this.ordersService.findOne(orderId);
+  }
+
+  @Get('user')
+  async findByUser(@User('sub') userId: string) {
+    return this.ordersService.findByUser(userId);
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Order } from './schemas/order.schema';
@@ -7,19 +7,32 @@ import { Order } from './schemas/order.schema';
 export class OrdersService {
   constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
 
-  async createOrder(orderData: any) {
+  async findAll(): Promise<Order[]> {
+    return this.orderModel.find().populate('user').populate('items.product');
+  }
+
+  async createOrder(orderData: any): Promise<Order> {
     return this.orderModel.create(orderData);
   }
 
-  async getOrder(orderId: string) {
+  async findOne(orderId: string): Promise<Order> {
     return this.orderModel.findById(orderId);
   }
 
-  async updateOrderStatus(orderId: string, status: string) {
-    return this.orderModel.findByIdAndUpdate(
+  async findByUser(userId: string): Promise<Order[]> {
+    return this.orderModel.find({ user: userId });
+  }
+
+  async updateOrderStatus(orderId: string, status: string): Promise<Order> {
+    const updatedOrder = await this.orderModel.findByIdAndUpdate(
       orderId,
       { status },
       { new: true },
     );
+
+    if (!updatedOrder)
+      throw new NotFoundException(`Orden con id ${orderId} no encontrada`);
+
+    return updatedOrder;
   }
 }
